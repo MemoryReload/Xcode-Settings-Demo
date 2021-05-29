a7fdfc8217b58fe74c441dfa2ce1f582c513e9b0
heping <heping@xiavan.com>
2021-05-29 18:17:04 +0800

    Initial Commit

---
+++ Shared/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  Shared
+//
+//  Created by 何平 on 2021/5/29.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
